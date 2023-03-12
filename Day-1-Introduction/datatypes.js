a843587fbff6de2c9b8a0794aff7f3c0a0ccc598
Veronicandemo <ndemoveronica@gmail.com>
2023-03-12 12:03:37 +0300

    30 Days of learning JavaScript

---
+++ Day-1-Introduction/datatypes.js
@@ -0,0 +1,33 @@
+//Create datatypes.js file and use the JavaScript typeof operator to check different data types. Check the data type of each variable
+
+typeof(variable1)
+typeof(variable2)
+typeof(variable3)
+typeof(variable4)
+
+//Declare four variables without assigning values
+let myName ;
+let mother;
+let myfriend;
+let myhobby;
+
+//Declare four variables with assigned values
+let mySchool = 'AkiraChix'
+let myBigSister = 'Wanda'
+let noOfStudents = 100
+let isGood = true
+
+//Declare variables to store your first name, last name, marital status, country and age in multiple lines
+let first_name = "Veronica"
+let lastName = 'Ndemo'
+let maritalStatus = 'Single'
+let country = 'Kenya'
+let age = 22
+
+//Declare two variables myAge and yourAge and assign them initial values and log to the browser console.I am 25 years old.
+//You are 30 years old.
+let myAge  = 21
+let yourAge = 24
+
+console.log(`I am ${myAge} years old`)
+console.log(`You are ${yourAge} yearold`)
---
+++ Day-1-Introduction/variable.js
@@ -0,0 +1,5 @@
+//Create a variable.js file and declare variables and assign string, boolean, undefined and null data types
+let variable1 = String;
+let variable2 = Boolean;
+let variable3 = undefined;
+let variable4 = null;
