d19223bacbf74c1e5a5da3c3f0ca246ab29613e4
Veronicandemo <ndemoveronica@gmail.com>
2023-03-20 22:32:30 +0300

    Conditionals

--- Day-4-conditionals/conditionals.js
+++ Day-4-conditionals/conditionals.js
@@ -1,65 +1,65 @@
 'use strict';
 
 //Statements used to make decisions based on differnt conditions
-// let a = 10;
-// if (a > 0) {
-//   console.log(`${a} is a positive number`);
-// } else if (a < 0) {
-//   console.log(`${a} is a negative number`);
-// } else if (a == 0) {
-//   console.log(`${a} is zero`);
-// } else {
-//   console.log(`${a} is not a number`);
-// }
+let aNum = 10;
+if (aNum > 0) {
+  console.log(`${aNum} is a positive number`);
+} else if (aNum < 0) {
+  console.log(`${aNum} is a negative number`);
+} else if (aNum == 0) {
+  console.log(`${aNum} is zero`);
+} else {
+  console.log(`${aNum} is not a number`);
+}
 
 //SWITCH - An alternativwe for if else if else else
 
-// let weather = 'Cloudy';
-// switch (weather) {
-//   case 'rainy':
-//     console.log('You need a raincoat');
-//     break;
-//   case 'Cloudy':
-//     console.log('Might be cold, You need a rain coat');
-//     break;
-//   case 'Sunny':
-//     console.log('You can get out freely');
-//     break;
-//   default:
-//     console.log('No need for rain coat');
-// }
-// let num = prompt('Enter number');
-// switch (true) {
-//   case num > 0:
-//     console.log('Number is positive');
-//     break;
-//   case num == 0:
-//     console.log('Numbers is zero');
-//     break;
-//   case num < 0:
-//     console.log('Number is negative');
-//     break;
-//   default:
-//     console.log('Entered value was not a number');
-// }
+let weather = 'Cloudy';
+switch (weather) {
+  case 'rainy':
+    console.log('You need a raincoat');
+    break;
+  case 'Cloudy':
+    console.log('Might be cold, You need a rain coat');
+    break;
+  case 'Sunny':
+    console.log('You can get out freely');
+    break;
+  default:
+    console.log('No need for rain coat');
+}
+let number = prompt('Enter number');
+switch (true) {
+  case number > 0:
+    console.log('Number is positive');
+    break;
+  case number == 0:
+    console.log('Numbers is zero');
+    break;
+  case number < 0:
+    console.log('Number is negative');
+    break;
+  default:
+    console.log('Entered value was not a number');
+}
 //Exercise LEVEL 1
 //Get user input using prompt(“Enter your age:”). If user is 18 or older , give feedback:'You are old enough to drive' but if not 18 give another feedback stating to wait for the number of years he needs to turn 18.
 
-// const age = Number(prompt('Enter your age:'));
-// if (age >= 18) {
-//   console.log('You are old enough to drive');
-// } else {
-//   console.log(`Wait for other ${18 - age} year to turn 18`);
-// }
+const age = Number(prompt('Enter your age:'));
+if (age >= 18) {
+  console.log('You are old enough to drive');
+} else {
+  console.log(`Wait for other ${18 - age} year to turn 18`);
+}
 //Compare the values of myAge and yourAge using if … else. Based on the comparison and log the result to console stating who is older (me or you). Use prompt(“Enter your age:”) to get the age as input.
 
-// const yourAge = 24;
-// const myAge = Number(prompt('Enter your age: '));
-// if (myAge < yourAge) {
-//   console.log(`You are ${yourAge - myAge} years younger than me`);
-// } else {
-//   console.log(`You are ${myAge - yourAge} years older than me`);
-// }
+const yourAge = 24;
+const myAge = Number(prompt('Enter your age: '));
+if (myAge < yourAge) {
+  console.log(`You are ${yourAge - myAge} years younger than me`);
+} else {
+  console.log(`You are ${myAge - yourAge} years older than me`);
+}
 
 //If a is greater than b return 'a is greater than b' else 'a is less than b'. Try to implement it in to ways
 
